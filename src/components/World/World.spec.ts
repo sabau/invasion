b3198453c8b18050c240b65d75e606a2836ec056
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-09-29 16:41:14 +0200

    Feature/read world (#2)
    * importing world
    * world validation

--- src/components/World/World.spec.ts
+++ src/components/World/World.spec.ts
@@ -1,18 +1,59 @@
 import {expect} from 'chai';
-import {initWorld, stringifyWorld} from './World';
+import {initWorld, parseCity, parseDirections, stringifyWorld, validateWorld} from './World';
 
 describe('Checking World', () => {
   describe('Ability to import world', () => {
-    const worldDescription = '';
 
     it('Should get empty world with empty string', () => {
-      const worldObject = initWorld(worldDescription);
-      expect(worldObject).to.be.deep.equal({cities: {}, roads: {}});
+      const worldObject = initWorld('');
+      expect(worldObject).to.be.deep.equal({});
     });
+
+    it('Should parse directions', () => {
+      const worldObject = parseDirections(['west=Foo', 'east=Bar']);
+      expect(worldObject).to.be.deep.equal({west: 'Foo', east: 'Bar'});
+    });
+
+
+    it('Should ignore wrong directions when parsing them', () => {
+      const worldObject = parseDirections(['westt=Foo', 'east=Bar']);
+      expect(worldObject).to.be.deep.equal({east: 'Bar'});
+    });
+
+    it('Should override duplicated directions when parsing them', () => {
+      const worldObject = parseDirections(['west=Foo', 'west=Bar']);
+      expect(worldObject).to.be.deep.equal({west: 'Bar'});
+    });
+
+    it('Should parseCity', () => {
+      const worldObject = parseCity('Foo');
+      expect(worldObject).to.be.deep.equal({Foo: {}});
+    });
+
+    it('Should parseCity with directions', () => {
+      const worldObject = parseCity('Foo west=Bar');
+      expect(worldObject).to.be.deep.equal({Foo: {west: 'Bar'}});
+    });
+
+    it('Should get a proper world', () => {
+      const worldObject = initWorld('./src/components/World/fixtures/world-looping');
+      expect(worldObject).to.be.deep.equal({Foo: {north: 'Bar'}, Bar: {south: 'Foo'}});
+    });
+
+    it('Should accept valid world', () => {
+      const worldObject = initWorld('./src/components/World/fixtures/world-looping');
+      expect(validateWorld(worldObject)).to.be.true;
+    });
+
+    it('Should reject invalid world', () => {
+      const worldObject = initWorld('./src/components/World/fixtures/world-inconsistent');
+      expect(validateWorld(worldObject)).to.be.false;
+    });
+
   });
 
   describe('Ability to print the world', () => {
-    const worldObject = {cities: {}, roads: {}};
+    const worldObject = {};
 
     it('should print an empty string if fed with empty world', () => {
       const worldString = stringifyWorld(worldObject);
--- src/components/World/World.ts
+++ src/components/World/World.ts
@@ -1,9 +1,40 @@
 import {Logger} from 'components/Logger';
 import {Dict} from 'dict';
+import * as fs from 'fs';
 
-export type World = {
-  cities: Dict<string>;
-  roads: Dict<string>;
+export type CityName = string;
+
+// We will destroy cities but not routes from neightbours. They will be lazily removed once
+// someone try to reach that city.
+export type City = Partial<Routes>;
+
+export enum Direction {
+  north = 'north',
+  south = 'south',
+  west = 'west',
+  east = 'east'
+}
+
+export type Routes = {
+  [key in keyof typeof Direction]: CityName;
+};
+
+export type World = Dict<City>;
+
+export const parseDirections = (directions: string[]) => directions.reduce(
+  (routes: Routes, route: string) => {
+    const routePieces = route.split('=');
+    return {
+      ...routes,
+      ...(routePieces.length === 2 && routePieces[0] in Direction &&
+        {[routePieces[0]]: routePieces[1]})
+    };
+  },
+  {});
+
+export const parseCity = (cityText: string): World => {
+  const cityList = cityText.split(' ');
+  return cityText.length > 0 ? {[cityList[0]]: {...parseDirections(cityList.slice(1))}} : {};
 };
 
 /**
@@ -12,8 +43,21 @@ export type World = {
  * @returns {World}
  */
 export const initWorld = (path: string): World => {
-  Logger.warn(path);
-  return {cities: {}, roads: {}};
+  let worldData = '';
+  if (fs.existsSync(path)) {
+    worldData = fs.readFileSync(path).toString();
+  }
+  return worldData.split('\n').reduce(
+    (world: World, currentCity: string) => ({...world, ...parseCity(currentCity)}), {});
+};
+
+export const validateWorld = (world: World) => {
+  // Every route defined in a city
+  return Object.values(world)
+    // should ve valid and each destination defined in those routes
+    .every(routes => !!(routes && Object.values(routes)
+      // Must point to a city that exists in the map
+      .every((destination) => !!(destination && world[destination]))));
 };
 
 /**
